26ca0184a396813bdaee3c78cf93c35a0fb4be02
Sileniz <walissonmacedo02@gmail.com>
2024-07-31 18:19:11 -0300

    minimal changes

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,10 +1,11 @@
 import JW from "@/assets/just-waifu.png"
 import TODOCLI from "@/assets/Todo-CLI.png"
 import YSPECS from "@/assets/Y-Specs.png"
-
+import SHUEISHA from "@/assets/Shueisa-CLI.png"
 const projects = [
   { text: "Just Waifu", src: JW },
   { text: "Todo-CLI", src: TODOCLI},
-  { text: "Y-Specs", src: YSPECS}
+  { text: "Y-Specs", src: YSPECS},
+  { text: "Shueisha-CLI", src: SHUEISHA}
 ]
 export default projects
