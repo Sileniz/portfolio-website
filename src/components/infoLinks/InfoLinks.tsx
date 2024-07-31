902e4f53e0bf43fd0c6fa1c01f463fc9c5750f7a
Sileniz <walissonmacedo02@gmail.com>
2024-07-31 19:34:37 -0300

    Add linkedin

--- src/components/infoLinks/InfoLinks.tsx
+++ src/components/infoLinks/InfoLinks.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image"
 import Link from "next/link"
 import github from "@/assets/25231.png"
+import linkedin from "@/assets/linkedin.png"
 import styles from "./InfoLinks.module.css"
 export default function InfoLinks() {
   return (
     <div className={styles.divLinks}>
-      <Image src={github} alt="github-icon" className={styles.Image}></Image>
-      <Link href="https://github.com/Sileniz" target="_blank" rel="noopener noreferrer" title="Github's link">/Github</Link>
+      <Link href="https://github.com/Sileniz" target="_blank" rel="noopener noreferrer" title="Github's link">
+        <Image src={github} alt="github-icon" className={styles.Image}></Image>
+      </Link>
+      <Link href="https://www.linkedin.com/in/walisson-da-silva-b71925297/" target="_blank" rel="noopener noreferrer" title="Linkedin's link">
+        <Image src={linkedin} alt="linkedin-icon" className={styles.Image}></Image>
+      </Link>
     </div>
   )
 }
