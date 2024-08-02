332e933f68c0ab17b54c59393ba4c42e270f5048
Sileniz <walissonmacedo02@gmail.com>
2024-08-02 01:05:36 -0300

    add github repo

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -4,11 +4,13 @@ import styles from "./Project.module.css"
 type ProjectProps = {
   text: string;
   src: any;
+  github: string;
 };
-export default function Project({text,src}: ProjectProps) {
+export default function Project({text,src,github}: ProjectProps) {
   return (
     <div className={styles.containerProject}>
       <p>{text}</p>
+      <a href={github} className={styles.githubLink}>Repositório</a>
       <Image src={src} alt="project's pic"></Image>
     </div>
   )
--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,11 +1,13 @@
-import JW from "@/assets/just-waifu.png"
-import TODOCLI from "@/assets/Todo-CLI.png"
-import YSPECS from "@/assets/Y-Specs.png"
-import SHUEISHA from "@/assets/Shueisa-CLI.png"
+import jw from "@/assets/just-waifu.png"
+import todocli from "@/assets/Todo-CLI.png"
+import yspects from "@/assets/Y-Specs.png"
+import shueisha from "@/assets/Shueisa-CLI.png"
+
 const projects = [
-  { text: "Just Waifu", src: JW },
-  { text: "Todo-CLI", src: TODOCLI},
-  { text: "Y-Specs", src: YSPECS},
-  { text: "Shueisha-CLI", src: SHUEISHA}
+  { text: "JustWaifu", src: jw, github: 'https://github.com/sileniz/just-waifu'},
+  { text: "Todo-CLI", src: todocli, github: 'https://github.com/sileniz/todo-cli'},
+  { text: "Y-specs", src: yspects, github: 'https://github.com/sileniz/y-specs'},
+  { text: "Shueisha-CLI", src: shueisha, github: 'https://github.com/sileniz/shueisha-cli'}
 ]
+
 export default projects
