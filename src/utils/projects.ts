691f048f2f7ab78bcea1583df906f143f6d49a80
Sileniz <walissonmacedo02@gmail.com>
2024-08-02 19:08:49 -0300

    _

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -4,7 +4,7 @@ import yspects from "@/assets/Y-Specs.png"
 import shueisha from "@/assets/Shueisa-CLI.png"
 
 const projects = [
-  { text: "JustWaifu", src: jw, github: 'https://github.com/sileniz/just-waifu', stack: "< React.JS | Javascript | HTML | CSS >"},
+  { text: "JustWaifu", src: jw, github: 'https://github.com/sileniz/just-waifu', stack: "< React.JS | Javascript | CSS >"},
   { text: "Todo-CLI", src: todocli, github: 'https://github.com/sileniz/todo-cli', stack: "< Node.JS | TypeScript >"},
   { text: "Y-specs", src: yspects, github: 'https://github.com/sileniz/y-specs', stack: "< Electron.JS | Javascript >"},
   { text: "Shueisha-CLI", src: shueisha, github: 'https://github.com/sileniz/shueisha-cli', stack: "< Node.JS | TypeScript >"}
