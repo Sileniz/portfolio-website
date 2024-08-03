cf1e321e96179a7b2cb3401d5b583b16a419a441
Sileniz <walissonmacedo02@gmail.com>
2024-08-03 00:03:11 -0300

    ..

--- src/components/infoLinks/InfoLinks.tsx
+++ src/components/infoLinks/InfoLinks.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Link from "next/link"
 import github from "@/assets/25231.png"
 import linkedin from "@/assets/linkedin.png"
-import cv from "@/assets/curriculum.svg"
+import curriculum from "@/assets/curriculum.svg"
 import styles from "./InfoLinks.module.css"
 
 export default function InfoLinks() {
@@ -14,8 +14,8 @@ export default function InfoLinks() {
       <Link href="https://www.linkedin.com/in/walisson-da-silva-b71925297/" target="_blank" rel="noopener noreferrer" title="Linkedin's link">
         <Image src={linkedin} alt="linkedin-icon" className={styles.Image}></Image>
       </Link>
-      <Link href="#" target="_blank" rel="noopener noreferrer" title="Linkedin's link">
-        <Image src={cv} alt="cv-icon" className={styles.Image}></Image>
+      <Link href="#" target="_blank" rel="noopener noreferrer" title="Curriculum's link">
+        <Image src={curriculum} alt="curriculum-icon" className={styles.Image}></Image>
       </Link>
     </div>
   )
